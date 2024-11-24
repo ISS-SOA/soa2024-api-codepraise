ISS-SOA/soa2021-api-codepraise

reverse-proxy caching of project appraisal

app/infrastructure/cache/redis_cache.rb
@@ -1,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'redis'
+
+module CodePraise
+  module Cache
+    # Redis client utility
+    class Client
+      def initialize(config)
+        @redis = Redis.new(url: config.REDISCLOUD_URL)
+      end
+
+      def keys
+        @redis.keys
+      end
+
+      def wipe
+        keys.each { |key| @redis.del(key) }
+      end
+    end
+  end
+end

ISS-SOA/soa2024-api-codepraise

app/infrastructure/cache/redis_cache.rb
@@ -1,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'redis'
+
+module CodePraise
+  module Cache
+    # Redis client utility
+    class Client
+      def initialize(config)
+        @redis = Redis.new(url: config.REDIS_URL)
+      end
+
+      def keys
+        @redis.keys
+      end
+
+      def wipe
+        keys.each { |key| @redis.del(key) }
+      end
+    end
+  end
+end